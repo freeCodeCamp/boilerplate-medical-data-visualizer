15e7cf1c9ae117b95a1a1baf253a21691eaf0c58
adamstok <adamstokowski@outlook.com>
2021-01-07 14:52:58 +0100

    overweight

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Import data
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # Add 'overweight' column
-df['overweight'] = None
+df['overweight'] = [
+    1 if x > 25 else 0 for x in df['weight'] / ((df['height']/100) ** 2)]
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholestorol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
 
@@ -17,13 +18,10 @@ def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
     df_cat = None
 
-
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the collumns for the catplot to work correctly.
     df_cat = None
 
     # Draw the catplot with 'sns.catplot()'
-
-
 
     # Do not modify the next two lines
     fig.savefig('catplot.png')
@@ -41,14 +39,10 @@ def draw_heat_map():
     # Generate a mask for the upper triangle
     mask = None
 
-
-
     # Set up the matplotlib figure
     fig, ax = None
 
     # Draw the heatmap with 'sns.heatmap()'
-
-
 
     # Do not modify the next two lines
     fig.savefig('heatmap.png')
