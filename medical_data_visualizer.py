b7449f2639ebbe2a48c45eb15b4ca5a535d4a891
Santi Knight <santiknight2003@gmail.com>
2024-06-19 14:29:11 +0000

    First additions to the project

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -3,19 +3,29 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+# Convert height from cm to meters
+df['height'] = df['height'] / 100
 
+# Calculate BMI
+df['BMI'] = df['weight'] / (df['height'] ** 2)
+
+# Determine overweight status
+df['overweight'] = (df['BMI'] > 25).astype(int)
+df = df.drop(columns=['BMI'])
 # 3
-
+df['cholesterol'] = df['cholesterol'].apply(lambda x: 0 if x == 1 else 1)
+df['gluc'] = df['gluc'].apply(lambda x: 0 if x == 1 else 1)
 
 # 4
 def draw_cat_plot():
-    # 5
-    df_cat = None
+    # Crear DataFrame en formato largo
+    df_cat = pd.melt(df, id_vars=['id', 'cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'], var_name='variable', value_name='value')
+    
+    # Crear el gráfico categórico
+    cat_plot = sns.catplot(data=df_cat, x='value', col='cardio', hue='variable', kind='count', height=4, aspect=1)
 
 
     # 6
